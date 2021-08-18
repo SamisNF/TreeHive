acfc7581bb15b5c6eb4193dfd2935cc0da97797b
SamisNF <sanefre@gmail.com>
2021-08-18 07:42:08 -0500

    Getting There
    Changes.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World")
+// multiples of the value entered into <input id="value1"> will return "Tree"
+// multiples of the value entered into <input id="value2"> will return "Hive"
+// multiples of both values entered in the inputs will return "Tree Hive"
+
+//control
+function getValues {
+    document.getElementById("value1").value;
+    document.getElementById("value2").value;
+
 }
+
+//view
+function generateOutput {
+
+}
+
+//display
+function displayOutput {
+
+
+}
+
+
