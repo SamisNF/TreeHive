184bd6734387a49e07679be23fbdb56ac19a7295
SamisNF <sanefre@gmail.com>
2021-08-18 21:28:57 -0500

    App Update
    Now I think I've created the app that's at least in the spirit of the coding exercise, if not exactly what I'm gonna see Davis demonstrate in the upcoming video

--- js/site.js
+++ js/site.js
@@ -2,32 +2,33 @@
 // multiples of the value entered into <input id="value2"> will return "Hive"
 // multiples of both values entered in the inputs will return "Tree Hive"
 
+// GLOBAL VARIABLES
+let value1 = document.getElementById("value1").value;
+let value2 = document.getElementById("value2").value;
 let numberSet = [];
 
-//control
-function getValues() {
-    let value1 = document.getElementById("value1").value;
-    let value2 = document.getElementById("value2").value;
-
+//CONTROL
+function getValues(value1, value2) {
+    
     value1 = parseInt(value1);
     value2 = parseInt(value2);
 
-    if(Number.isInteger(value1) && Number.isInteger(value2)) {
+    if(Number.isInteger(value1) && Number.isInteger(value2) && value1 > 0 && value1 < 11 && value2 > 0 && value2 < 11) {
 
-        numberSet = generateNumbers(value1, value2);
+        numberSet = generateNumbers();
 
         displayOutput(numberSet);
 
     } else {
-        alert("You must enter integers.");
+        alert("You must enter whole numbers from 1 to 10.");
     }
 
     
 }
 
 //view
-function generateNumbers(value1, value2) {
-    for (let index = value1; index <= value2; index++) {
+function generateNumbers() {
+    for (let index = 1; index <= 100; index++) {
         numberSet.push(index);
     }
 
@@ -35,7 +36,7 @@ function generateNumbers(value1, value2) {
 }
 
 //display
-function displayOutput(numberSet) {
+function displayOutput(numberSet, value1, value2) {
 
     let templateRows = "";
 
@@ -44,13 +45,13 @@ function displayOutput(numberSet) {
         let number = numberSet[index];
         let className = "";
 
-        if (number % 3 == 0 && number % 5 == 0) {
+        if (number % value1 == 0 && number % value2 == 0) {
             number = "Tree Hive";
             className = "treeHive";
-        } else if (number % 3 == 0) {
+        } else if (number % value1 == 0) {
             number = "Tree";
             className = "tree";
-        } else if (number % 5 == 0) {
+        } else if (number % value2 == 0) {
             number = "Hive";
             className = "hive";
         } else {
